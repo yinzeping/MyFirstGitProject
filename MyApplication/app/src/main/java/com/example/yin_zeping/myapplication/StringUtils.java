65043416ec8c47e4de6f8572cb526aa760792e60
yinzeping <yin_zeping@163.com>
2016-03-01 11:58:03 +0800

    add new code

---
+++ MyApplication/app/src/main/java/com/example/yin_zeping/myapplication/StringUtils.java
@@ -0,0 +1,11 @@
+package com.example.yin_zeping.myapplication;
+
+/**
+ * Created by yin_zeping on 2016/3/1.
+ */
+public class StringUtils {
+
+    public static String subString(String s) {
+        return s.substring(0, 2);
+    }
+}
